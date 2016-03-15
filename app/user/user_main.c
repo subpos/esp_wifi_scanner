1c88a0728f4a37fb0c7f5ee82ddbf2ae3f2544d7
Blair Wyatt <blecky@subpos.org>
2016-03-15 12:58:28 +1100

    Update SDK and scan all channels individually. For some reason, scanning all channels doesn't scan everything.

--- app/user/user_main.c
+++ app/user/user_main.c
@@ -30,16 +30,22 @@
 #define printf(...) os_printf( __VA_ARGS__ )
 #define sprintf(...) os_sprintf( __VA_ARGS__ )
 
+#define MAX_CHAN 13 //Setting to 14 overflows to 0 for some reason
+
 
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void user_procTask(os_event_t *events);
 long clockCounter = 0;
 
-LOCAL os_timer_t trilaterate_timer;
+struct scan_config scan_conf;
+
+LOCAL os_timer_t scan_timer;
 
 uint8  ssid[30][33];  //30 ssids
 uint16 rssi[30];
 uint8  chan[30];
+int i = 0; 
+int current_chan = 1;
 
 int scan_complete;
 
@@ -48,13 +54,13 @@ void user_rf_pre_init(void) {}
 static void ICACHE_FLASH_ATTR
 scan_done(void* arg,STATUS status)
 {
-    int i = 0; 
+    
     //ets_uart_printf("%d\r\n",status);
     if (status == OK)
     {
        
         struct bss_info *bss_link = (struct bss_info *)arg;
-        bss_link = bss_link->next.stqe_next; //ignore first
+        bss_link = bss_link->next.stqe_next; //Get first SSID
         
         while (bss_link != NULL && i < 30) //Max 30 nodes.
         {
@@ -63,35 +69,21 @@ scan_done(void* arg,STATUS status)
                 bss_link->ssid[1] == 'P' && 
                 bss_link->ssid[2] == 'S') 
             {
-                    //os_memset(ssid, 0, 33);
-                    if (os_strlen(bss_link->ssid) <= 32)
-                    {
-                        os_memcpy(ssid[i], bss_link->ssid, os_strlen(bss_link->ssid));
-                        chan[i] = bss_link->channel;
-                        rssi[i] = bss_link->rssi;
-                        i++;
-                    }
+                //os_memset(ssid, 0, 33);
+                if (os_strlen(bss_link->ssid) <= 32)
+                {
+                    os_memcpy(ssid[i], bss_link->ssid, os_strlen(bss_link->ssid));
+                    chan[i] = bss_link->channel;
+                    rssi[i] = bss_link->rssi;
+					//ets_uart_printf("SPS Found:  %d\r\n",chan[i]);
+                    i++;
+					
+                }
             }
 
             bss_link = bss_link->next.stqe_next;
         }
-        if (i > 0) {
-            ets_uart_printf("S\r\n"); //Start
-            ets_uart_printf("%d\r\n",i); //Print number of items in scan
-            while (i > 0)
-            {
-                int j = 0;
-                while (j < 31) // Subpos SSIDs are 31 long
-                {
-                    ets_uart_printf("%02x",ssid[i-1][j]);
-                    j++;
-                }
-                ets_uart_printf(",%d,%d\r\n",rssi[i-1],chan[i-1]);
-                i--;
-            }
-            
-            ets_uart_printf("E\r\n"); //End
-        }
+        
     }
     scan_complete = true;
     
@@ -99,11 +91,40 @@ scan_done(void* arg,STATUS status)
 
 void scanner(void *arg)
 {
+	scan_conf.channel = current_chan;
+	//This prevents another scann happening before the previous has stopped.
     if (scan_complete)
     {
-        scan_complete = false;
-        wifi_station_scan(NULL,scan_done);
-    }
+		if (current_chan <= MAX_CHAN) {
+			scan_complete = false;
+			wifi_station_scan(&scan_conf,scan_done);
+			
+			current_chan++;
+        }
+		else 
+		{
+			if (i > 0) {
+				ets_uart_printf("S\r\n"); //Start
+				ets_uart_printf("%d\r\n",i); //Print number of items in Node scan
+				while (i > 0)
+				{
+					int j = 0;
+					while (j < 31) // Subpos SSIDs are 31 long
+					{
+						ets_uart_printf("%02x",ssid[i-1][j]);
+						j++;
+					}
+					ets_uart_printf(",%d,%d\r\n",rssi[i-1],chan[i-1]);
+					i--;
+				}
+				
+				ets_uart_printf("E\r\n"); //End
+			}
+			
+			current_chan = 1;	
+			i = 0;
+		}
+	}
 }
 
 
@@ -119,26 +140,40 @@ user_procTask(os_event_t *events)
 void ICACHE_FLASH_ATTR
 user_init(void)
 {
+
+    char ssid[32] = {0};
     scan_complete = true;
     
     // Configure the UART0 and UART1 (TX only) to 9600
 	uart_init(BIT_RATE_115200,BIT_RATE_115200);
+	
+	scan_conf.ssid = NULL;
+	scan_conf.bssid = NULL;
+	scan_conf.show_hidden = 1;
+	
     
     //Set station mode
     wifi_set_opmode_current( STATION_MODE );
+	
+    struct station_config conf;
+    wifi_station_get_config_default(&conf);
+	os_memcpy(&conf.ssid, ssid, 32);
+	conf.bssid_set = 0;
+	
+	wifi_station_set_config_current(&conf);
     
     //Disarm timer
-    os_timer_disarm(&trilaterate_timer);
+    os_timer_disarm(&scan_timer);
 
     //Setup timer
-    os_timer_setfn(&trilaterate_timer, (os_timer_func_t *)scanner, NULL);
+    os_timer_setfn(&scan_timer, (os_timer_func_t *)scanner, NULL);
 
-    //&trilaterate_timer is callback
+    //&scan_timer is callback
     //delay in ms
     //0 for once and 1 for repeating
     //This callback timer value can't be too small as a new scan will start before the previous finishes. 
     //The callback function checks if the scan is complete before starting a new scan.
-    os_timer_arm(&trilaterate_timer, 10, 1); //100Hz
+    os_timer_arm(&scan_timer, 10, 1); //100Hz
     
     //Start os task
     system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
--- include/user_interface.h
+++ include/user_interface.h
@@ -23,11 +23,13 @@
 #endif
 
 enum rst_reason {
-	DEFAULT_RST_FLAG	= 0,
-	WDT_RST_FLAG	= 1,
-	EXCEPTION_RST_FLAG    = 2,
-	SOFT_RST_FLAG   = 3,
-	DEEP_SLEEP_AWAKE_FLAG	= 4
+	REASON_DEFAULT_RST		= 0,
+	REASON_WDT_RST			= 1,
+	REASON_EXCEPTION_RST	= 2,
+	REASON_SOFT_WDT_RST   	= 3,
+	REASON_SOFT_RESTART 	= 4,
+	REASON_DEEP_SLEEP_AWAKE	= 5,
+	REASON_EXT_SYS_RST      = 6
 };
 
 struct rst_info{
@@ -39,6 +41,8 @@ struct rst_info{
 	uint32 excvaddr;
 	uint32 depc;
 };
+
+struct rst_info* system_get_rst_info(void);
 
 #define UPGRADE_FW_BIN1         0x00
 #define UPGRADE_FW_BIN2         0x01
@@ -129,6 +133,13 @@ void system_phy_set_max_tpw(uint8 max_tpw);
 void system_phy_set_tpw_via_vdd33(uint16 vdd33);
 void system_phy_set_rfoption(uint8 option);
 
+bool system_param_save_with_protect(uint16 start_sec, void *param, uint16 len);
+bool system_param_load(uint16 start_sec, uint16 offset, void *param, uint16 len);
+
+void system_soft_wdt_stop(void);
+void system_soft_wdt_restart(void);
+void system_soft_wdt_feed(void);
+
 #define NULL_MODE       0x00
 #define STATION_MODE    0x01
 #define SOFTAP_MODE     0x02
@@ -159,6 +170,7 @@ struct bss_info {
     sint8 rssi;
     AUTH_MODE authmode;
     uint8 is_hidden;
+    sint16 freq_offset;
 };
 
 typedef struct _scaninfo {
@@ -229,6 +241,9 @@ bool wifi_station_dhcpc_start(void);
 bool wifi_station_dhcpc_stop(void);
 enum dhcp_status wifi_station_dhcpc_status(void);
 
+char* wifi_station_get_hostname(void);
+bool wifi_station_set_hostname(char *name);
+
 struct softap_config {
     uint8 ssid[32];
     uint8 password[64];
@@ -270,6 +285,7 @@ void wifi_softap_free_station_info(void);
 bool wifi_softap_dhcps_start(void);
 bool wifi_softap_dhcps_stop(void);
 bool wifi_softap_set_dhcps_lease(struct dhcps_lease *please);
+bool wifi_softap_get_dhcps_lease(struct dhcps_lease *please);
 enum dhcp_status wifi_softap_dhcps_status(void);
 bool wifi_softap_set_dhcps_offer_option(uint8 level, void* optarg);
 
@@ -408,5 +424,27 @@ typedef struct _esp_event {
 typedef void (* wifi_event_handler_cb_t)(System_Event_t *event);
 
 void wifi_set_event_handler_cb(wifi_event_handler_cb_t cb);
+
+typedef enum wps_type {
+	WPS_TYPE_DISABLE = 0,
+	WPS_TYPE_PBC,
+	WPS_TYPE_PIN,
+	WPS_TYPE_DISPLAY,
+	WPS_TYPE_MAX,
+} WPS_TYPE_t;
+
+enum wps_cb_status {
+	WPS_CB_ST_SUCCESS = 0,
+	WPS_CB_ST_FAILED,
+	WPS_CB_ST_TIMEOUT,
+	WPS_CB_ST_WEP,
+};
+
+bool wifi_wps_enable(WPS_TYPE_t wps_type);
+bool wifi_wps_disable(void);
+bool wifi_wps_start(void);
+
+typedef void (*wps_st_cb_t)(int status);
+bool wifi_set_wps_cb(wps_st_cb_t cb);
 
 #endif
